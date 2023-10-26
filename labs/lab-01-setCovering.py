4281b3213f964a310d66a3c43088e5f12b6d49e9
Alessandro Chiabodo <alessandro.chiabodo@gmail.com>
2023-10-26 19:39:18 +0200

    UPDATE (lab) - updated euristic function

--- labs/lab-01-setCovering.py
+++ labs/lab-01-setCovering.py
@@ -6,8 +6,8 @@ from random import random
 from queue import PriorityQueue, SimpleQueue , LifoQueue
 from functools import reduce
 from enum import Enum
-PROBLEM_SIZE = 5
-NUM_SETS = 15
+PROBLEM_SIZE = 20
+NUM_SETS = 25
 
 State = namedtuple('State', ['taken', 'untaken'])
 
@@ -16,10 +16,16 @@ class Algorithm(Enum):
     GREEDY = 2
     DEPTH_FIRST = 3
     BREADTH_FIRST = 4
+    DIJKSTRA = 5
 
 class SetCoveringProblem:
     
-    def __init__(self,algorithm : Algorithm) -> None:
+    class Mode(Enum):
+        ACTION = 1
+        DEBUG = 2
+
+    def __init__(self,algorithm : Algorithm,MODE : Mode = Mode.DEBUG) -> None:
+        self.mode = MODE
         match algorithm:
             case Algorithm.A_STAR:
                 self.frontier = PriorityQueue()
@@ -27,12 +33,12 @@ class SetCoveringProblem:
             case Algorithm.GREEDY:
                 self.frontier = PriorityQueue()
                 self.cost_function = self.distance
-            case Algorithm.DEPTH_FIRST:
-                self.frontier = LifoQueue() #it would be more correct to use deque() but it does not allow you to use the put method
-                self.cost_function = lambda state : 0
             case Algorithm.BREADTH_FIRST:
                 self.frontier = SimpleQueue()
                 self.cost_function = lambda state : 0
+            case Algorithm.DIJKSTRA:
+                self.frontier = PriorityQueue()
+                self.cost_function = lambda state : state.taken.__len__()
         
     def goal_check(self,state):
         return np.all(self.covered(state))
@@ -41,17 +47,25 @@ class SetCoveringProblem:
         return reduce(np.logical_or,[self.sets[i] for i in state.taken],  np.array([False for _ in range(self.size)]) )
 
     def distance(self,state):
-        return self.size - sum (
-            reduce(np.logical_or,[self.sets[i] for i in state.taken], np.array([False for _ in range(self.size)]) ))
+        return self.size - sum (reduce(np.logical_or,[self.sets[i] for i in state.taken], np.array([False for _ in range(self.size)]) ))
 
     def euristic(self,state):
         already_covered = self.covered(state)
         if np.all(already_covered):
             return 0
-        largest_remaining_set_size = max(sum(np.logical_and(s, np.logical_not(already_covered))) for s in self.sets if not np.all(np.logical_and(s, state.taken)))
-        missing_size = self.size - sum(already_covered)
-        optimistic_estimate = ceil(missing_size / largest_remaining_set_size)
-        return optimistic_estimate
+        optimistic_estimate = 0
+        # Sort sets by number of elements not already covered
+        untaken_sets = sorted((sum(np.logical_and(s, np.logical_not(already_covered))) for s in [self.sets[i] for i in state.untaken]), reverse=True)
+        
+        # Add sets until all elements are covered
+        for set in untaken_sets:
+            missing_size = self.size - sum(already_covered)
+            if missing_size <= 0:
+                break
+            if not np.all(np.logical_and(set, already_covered)):
+                optimistic_estimate += 1
+                already_covered = np.logical_or(already_covered, set)  
+        return optimistic_estimate + state.taken.__len__()
 
     def check_solvable(self,sets):
         self.sets = sets
@@ -80,26 +94,39 @@ class SetCoveringProblem:
                 self.frontier.put((self.cost_function(self.new_state),self.new_state))
             _ , self.current_state = self.frontier.get()
         if(self.goal_check(self.current_state)):
-            print("Solution found")
-            print(f"Found solution in {format(self.counter)} iterations,  steps : ({len(self.current_state.taken)})")
-            print(f"Solution : {self.current_state.taken}")
+            if self.mode == self.Mode.DEBUG:
+                print("Solution found")
+                print(f"Found solution in {format(self.counter)} iterations,  steps : ({len(self.current_state.taken)})")
+                print(f"Solution : {self.current_state.taken}")
+            return self.current_state.taken
         else:
-            print("No solution found")
+            return None
+
 
 if __name__ == "__main__":
+    compare = Algorithm.GREEDY
+    MODE = SetCoveringProblem.Mode.ACTION
+    PROBABILITY = 0.25
+    if PROBABILITY < 0 or PROBABILITY > 1:
+        raise Exception("Probability must be between 0 and 1")
     
-    SETS = tuple([np.array([random() < .1 for _ in range(PROBLEM_SIZE)]) for _ in range(NUM_SETS)])
-    while (SetCoveringProblem(Algorithm.A_STAR).check_solvable(SETS) == False):
-        SETS = tuple([np.array([random() < .1 for _ in range(PROBLEM_SIZE)]) for _ in range(NUM_SETS)])
+    for i in range(100):    
+        SETS = tuple([np.array([random() < PROBABILITY for _ in range(PROBLEM_SIZE)]) for _ in range(NUM_SETS)])
+        while (SetCoveringProblem(Algorithm.A_STAR).check_solvable(SETS) == False):
+            SETS = tuple([np.array([random() < PROBABILITY for _ in range(PROBLEM_SIZE)]) for _ in range(NUM_SETS)])
+        print("iteration : ",i)
 
-    print("A*")
-    SetCoveringProblem(Algorithm.A_STAR).solve(SETS)
-
-    print("greedy")
-    SetCoveringProblem(Algorithm.GREEDY).solve(SETS)
-
-    print("depth first")
-    SetCoveringProblem(Algorithm.DEPTH_FIRST).solve(SETS)
-
-    print("breadth first")
-    SetCoveringProblem(Algorithm.BREADTH_FIRST).solve(SETS)
+        stateA = SetCoveringProblem(Algorithm.A_STAR,MODE=MODE).solve(SETS)
+        match compare:
+            case Algorithm.GREEDY:
+                stateB = SetCoveringProblem(Algorithm.GREEDY,MODE=MODE).solve(SETS)
+            case Algorithm.DIJKSTRA:
+                stateB = SetCoveringProblem(Algorithm.DIJKSTRA,MODE=MODE).solve(SETS)
+            case Algorithm.BREADTH_FIRST:
+                stateB = SetCoveringProblem(Algorithm.BREADTH_FIRST,MODE=MODE).solve(SETS)
+        if stateA.__len__() > stateB.__len__():
+            print("ERROR")
+            print(SETS)
+            print(stateA)
+            print(stateB)
+            break
