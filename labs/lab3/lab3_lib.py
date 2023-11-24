ddb5a98a76ac7a2ae8e10a9b7e939b95d81709ec
ALESSANDRO CHIABODO s309234 <s309234@studenti.polito.it>
2023-11-24 17:11:33 +0100

    update (labs) - updated lib

--- labs/lab3/lab3_lib.py
+++ labs/lab3/lab3_lib.py
@@ -4,6 +4,7 @@
 
 from abc import abstractmethod
 
+
 class AbstractProblem:
     def __init__(self):
         self._calls = 0
@@ -23,9 +24,12 @@ class AbstractProblem:
 
     def __call__(self, genome):
         self._calls += 1
-        fitnesses = sorted((AbstractProblem.onemax(genome[s::self.x]) for s in range(self.x)), reverse=True)
-        val = fitnesses[0] - sum(f*(.1 ** (k+1)) for k, f in enumerate(fitnesses[1:]))
-        return val / len(genome) * self.x
+        fitnesses = sorted((AbstractProblem.onemax(genome[s :: self.x]) for s in range(self.x)), reverse=True)
+        val = sum(f for f in fitnesses if f == fitnesses[0]) - sum(
+            f * (0.1 ** (k + 1)) for k, f in enumerate(f for f in fitnesses if f < fitnesses[0])
+        )
+        return val / len(genome)
+
 
 def make_problem(a):
     class Problem(AbstractProblem):
@@ -35,4 +39,3 @@ def make_problem(a):
             return a
 
     return Problem()
-
